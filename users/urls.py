ed707802d2d163eada81f7c72a4c012e4e353651
Miguel Mark <miguel12.mark@gmail.com>
2026-04-24 18:37:20 -0500

    saved users/urls

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import perfil
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import perfil, UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('perfil/', perfil),
+    path('', include(router.urls)),
 ]
