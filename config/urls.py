b7d655d9a5c0bf277d0ff26858cbd87ed4d8c7d0
Miguel Mark <miguel12.mark@gmail.com>
2026-04-25 18:20:25 -0500

    fix urls

--- config/urls.py
+++ config/urls.py
@@ -4,23 +4,28 @@ from django.urls import path, include
 from users.views import CustomTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import login_view
+from .views import login_view, home
 
 urlpatterns = [
-    path('', login_view),
-    
+    # 🔐 LOGIN
+    path('login/', login_view),
+
+    # 🏠 HOME
+    path('', home),
+
     path('admin/', admin.site.urls),
 
-    # 🔐 AUTH
+    # 🔐 AUTH API
     path('api/token/', CustomTokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
 
-    # 📦 APPS
+    # 🔗 API
     path('api/', include('users.urls')),
     path('api/', include('clientes.urls')),
     path('api/', include('productos.urls')),
     path('api/', include('licitaciones.urls')),
-    
+
+    # 🔥 HTML VIEWS
     path('', include('clientes.urls')),
     path('', include('productos.urls')),
     path('', include('licitaciones.urls')),
--- config/views.py
+++ config/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def login_view(request):
     return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'home.html')
