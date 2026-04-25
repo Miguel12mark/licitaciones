809d17e68f93f81e4cd37657c72db7478385ef3e
Miguel Mark <miguel12.mark@gmail.com>
2026-04-25 18:26:58 -0500

    add logout

--- config/views.py
+++ config/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 def login_view(request):
     return render(request, 'login.html')
 
 def home(request):
+    # 🔒 si no hay cookie → login
+    if not request.COOKIES.get('logged'):
+        return redirect('/login/')
+
     return render(request, 'home.html')
